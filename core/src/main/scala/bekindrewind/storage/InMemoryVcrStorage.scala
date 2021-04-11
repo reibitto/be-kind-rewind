8a7e8145bccbb0ecccbcdbe25f546eca0167d5b6
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-04-11 16:34:22 +0900

    Use InMemoryVcrStorage for testing

---
+++ core/src/main/scala/bekindrewind/storage/InMemoryVcrStorage.scala
@@ -0,0 +1,21 @@
+package bekindrewind.storage
+
+import bekindrewind.VcrEntries
+
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ * Intended for testing purpose.
+ * Do not use in production.
+ */
+final class InMemoryVcrStorage() extends VcrStorage {
+
+  private val entriesRef: AtomicReference[VcrEntries] = new AtomicReference[VcrEntries]()
+
+  override def read(): Either[Throwable, VcrEntries] = entriesRef.getAcquire match {
+    case null    => Left(new IllegalStateException("storage is empty"))
+    case entries => Right(entries)
+  }
+
+  override def write(entries: VcrEntries): Unit = entriesRef.set(entries)
+}
--- core/src/test/scala/bekindrewind/VcrClientSpec.scala
+++ core/src/test/scala/bekindrewind/VcrClientSpec.scala
@@ -1,20 +1,17 @@
 package bekindrewind
 
-import io.circe.parser._
-import io.circe.syntax._
+import bekindrewind.storage.{ InMemoryVcrStorage, VcrStorage }
 import munit._
 
 import java.net.URI
-import java.nio.charset.StandardCharsets
-import java.nio.file._
 import java.time.{ Duration, OffsetDateTime }
 import scala.collection.immutable
 
 class VcrClientSpec extends FunSuite {
 
   test("Request and response are saved as JSON") {
-    val recordingPath = Files.createTempFile("test", ".json")
-    val client        = MockClient(recordingPath, RecordOptions.default, VcrMatcher.default)
+    val storage = new InMemoryVcrStorage()
+    val client  = MockClient(storage, RecordOptions.default, VcrMatcher.default)
     assert(client.previouslyRecorded.isEmpty)
     assert(client.newlyRecorded().isEmpty)
 
@@ -28,15 +25,14 @@ class VcrClientSpec extends FunSuite {
     assert(client.previouslyRecorded.isEmpty)
     assertEquals(client.newlyRecorded().size, 1)
 
-    val savedJson = new String(Files.readAllBytes(recordingPath), StandardCharsets.UTF_8)
-    val decoded   = decode[VcrEntries](savedJson).map(_.entries)
+    val decoded = storage.read().map(_.entries)
     assertEquals(decoded, Right(Vector(entry)))
   }
 
   test("Request and response can be transformed") {
-    val recordingPath = Files.createTempFile("test", ".json")
-    val client        = MockClient(
-      recordingPath,
+    val storage = new InMemoryVcrStorage()
+    val client  = MockClient(
+      storage,
       RecordOptions.default,
       VcrMatcher.identity.withTransformer { case entry @ VcrEntry(req, res, _) =>
         entry.copy(
@@ -61,8 +57,7 @@ class VcrClientSpec extends FunSuite {
     assertEquals(client.newlyRecorded(), immutable.Seq(expected))
 
     client.save()
-    val savedJson = new String(Files.readAllBytes(recordingPath), StandardCharsets.UTF_8)
-    val decoded   = decode[VcrEntries](savedJson).map(_.entries)
+    val decoded = storage.read().map(_.entries)
     assertEquals(decoded, Right(Vector(expected)))
   }
 
@@ -72,12 +67,12 @@ class VcrClientSpec extends FunSuite {
       VcrResponse(200, "ok", Map.empty, "{}", Some("text/json")),
       OffsetDateTime.parse("2100-05-06T12:34:56.789Z")
     )
-    val rawJson = VcrEntries(Vector(entry), BuildInfo.version).asJson.spaces2
+    val entries = VcrEntries(Vector(entry), BuildInfo.version)
 
-    val recordingPath = Files.createTempFile("test", ".json")
-    Files.write(recordingPath, rawJson.getBytes(StandardCharsets.UTF_8))
+    val storage = new InMemoryVcrStorage()
+    storage.write(entries)
 
-    val client = MockClient(recordingPath, RecordOptions.default, VcrMatcher.groupBy(_ => "bucket"))
+    val client = MockClient(storage, RecordOptions.default, VcrMatcher.groupBy(_ => "bucket"))
     assert(client.newlyRecorded().isEmpty)
 
     client.previouslyRecorded.get(VcrKey("bucket")) match {
@@ -89,9 +84,9 @@ class VcrClientSpec extends FunSuite {
   }
 
   test("Record file should have expiration field if option specified") {
-    val recordingPath = Files.createTempFile("test", ".json")
-    val client        = MockClient(
-      recordingPath,
+    val storage = new InMemoryVcrStorage()
+    val client  = MockClient(
+      storage,
       RecordOptions.default.copy(
         expiresAfter = Some(Duration.ofDays(90))
       ),
@@ -108,8 +103,7 @@ class VcrClientSpec extends FunSuite {
     client.addNewEntry(entry)
     client.save()
 
-    val savedJson = new String(Files.readAllBytes(recordingPath), StandardCharsets.UTF_8)
-    val decoded   = decode[VcrEntries](savedJson)
+    val decoded = storage.read()
     assertEquals(decoded.map(_.entries), Right(Vector(entry)))
     assert(decoded.toOption.flatMap(_.expiration).isDefined)
   }
@@ -120,19 +114,19 @@ class VcrClientSpec extends FunSuite {
       VcrResponse(200, "ok", Map.empty, "{}", Some("text/json")),
       OffsetDateTime.parse("2100-05-06T12:34:56.789Z")
     )
-    val rawJson = VcrEntries(
+    val entries = VcrEntries(
       Vector(entry),
       BuildInfo.version,
       expiration = Some(OffsetDateTime.parse("2010-01-01T12:00:00Z"))
-    ).asJson.spaces2
+    )
 
-    val recordingPath = Files.createTempFile("test", ".json")
-    Files.write(recordingPath, rawJson.getBytes(StandardCharsets.UTF_8))
+    val storage = new InMemoryVcrStorage()
+    storage.write(entries)
 
-    val client = MockClient(recordingPath, RecordOptions.default, VcrMatcher.groupBy(_ => "bucket"))
+    val client = MockClient(storage, RecordOptions.default, VcrMatcher.groupBy(_ => "bucket"))
     assert(client.newlyRecorded().isEmpty)
     assertEquals(client.previouslyRecorded.get(VcrKey("bucket")), None)
   }
 }
 
-final case class MockClient(recordingPath: Path, recordOptions: RecordOptions, matcher: VcrMatcher) extends VcrClient
+final case class MockClient(storage: VcrStorage, recordOptions: RecordOptions, matcher: VcrMatcher) extends VcrClient
